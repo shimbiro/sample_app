4c0d4e09e71763dc1b6fdbabc5deef2aae7802e5
shimbiro <shimbirosharon@gmail.com>
2016-10-28 15:33:59 +0300

    Finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,         name
+    assert_equal full_title("Help"), email
+  end
+end
